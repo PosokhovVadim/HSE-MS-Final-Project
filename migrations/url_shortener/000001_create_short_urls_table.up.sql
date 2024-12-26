576291a275e62c01e8b0268bc18f46b0a1b0df19
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-12-26 06:38:30 +0400

    fixes oops:)

--- migrations/url_shortener/000001_create_short_urls_table.up.sql
+++ migrations/url_shortener/000001_create_short_urls_table.up.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE short_urls IF NOT EXISTS (
     id INTEGER PRIMARY KEY,
     original_url varchar(1024) NOT NULL,
     short_url varchar(1024) NOT NULL
-)
+);
