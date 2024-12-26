90662dc899b9abf878d7822c96836b926b1446cd
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-12-26 04:25:55 +0400

    init imple-ed

--- pkg/Config/config.py
+++ pkg/Config/config.py
@@ -4,16 +4,16 @@ import logging
 
 class Config:
     def __init__(self, config_path, log: logging.Logger):
-        self.config = self.readConfig(config_path)
+        self.config = self.readConfig(config_path,log)
         self.service_name = self.config["service_name"]
         self.host = self.config["http_server"]["host"]
         self.port = self.config["http_server"]["port"]
-        self.log = log
-    def readConfig(self, config_path):
+
+    def readConfig(self, config_path, log: logging.Logger):
         try:
             with open(config_path, "r") as config_file:
-                self.log.info("Reading config file successfully")
+                log.info("Reading config file successfully")
                 return yaml.safe_load(config_file)
         except IOError:
-            self.log.error("Config file not found")
+            log.error("Config file not found")
             raise IOError
--- url_shortener/main.py
+++ url_shortener/main.py
@@ -1,2 +1,30 @@
+import os
+from fastapi import FastAPI
+from pkg.Config.config import Config
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+# from todo_service.context import ctx
+import uvicorn
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+log = logging.getLogger("todo_service.log")
+cfg = Config(os.getenv("CONFIG_PATH"), log)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 def main():
-    print("Hello shortener")
+    log.info(f"Config: {cfg.host}:{cfg.port}")
+    uvicorn.run("todo_service.main:app", host=cfg.host, port=cfg.port, reload=True)
